9b211036da40952531c110ab7f459b8b9c2da201
Jade <jade@meteor.com>
2018-01-07 15:41:23 -0800

    first commit

---
+++ lightswitch_transmitter.ino
@@ -0,0 +1,19 @@
+/* Project Wireless Lightswitch
+ *  Purpose: wirelessly turn off LED's on top of 2 game stations
+ *  How it works: USB connected to lightswitch outlet will output 5V or 0V.
+ *  Upon receiving 5V, transmit RF signal to turn on 2 outlets (that have
+ *  LED's connected). Upon 5V -> 0V, transmit RF signal to turn them off.
+*/
+
+float input_voltage = 0.0;
+float temp=0.0;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
